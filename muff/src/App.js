699e1b554697e1592d26d853f2d2af04a799e490
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2023-03-01 22:28:08 +0000

    still borring design

--- muff/src/App.js
+++ muff/src/App.js
@@ -1,5 +1,15 @@
+import React from 'react'
+import Navbar from './components/nav/Navbar'
 function App() {
-  return <div className="App">muf</div>
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>vfbvfbfgbgf</h1>
+      <h1>vfbvfbfgbgf</h1>
+      <h1>vfbvfbfgbgf</h1>
+      <h1>vfbvfbfgbgf</h1>
+    </div>
+  )
 }
 
 export default App
---
+++ muff/src/components/nav/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+// import { Link } from 'react-router-dom'
+import './Navbar.scss'
+
+const Navbar = () => {
+  const [active, setActive] = useState(false)
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false)
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', isActive)
+    return () => {
+      window.removeEventListener('scroll', isActive)
+    }
+  }, [])
+  return (
+    <div className={active ? 'navbar active' : 'navbar'}>
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">g and B</span>
+          {/* </Link> */}
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>sign in</span>
+          <span>become a seller</span>
+          <button>join</button>
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>TEST 2</span>
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Navbar
